d8c058d8405e8d7557f3dd6fed3e218af96d7e7b
Rushabh <rushabhshah341@gmail.com>
2017-04-13 20:26:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const c = require('./lib/constants');
+
+module.exports.category = c.category;
+module.exports.collection = c.collection;
+module.exports.sort = c.sort;
+module.exports.age = c.age;
+
+module.exports.app = require('./lib/app');
+module.exports.list = require('./lib/list');
+module.exports.search = require('./lib/search');
+module.exports.suggest = require('./lib/suggest');
+module.exports.developer = require('./lib/developer');
+module.exports.reviews = require('./lib/reviews');
+module.exports.similar = require('./lib/similar');
+module.exports.permissions = require('./lib/permissions');
+
+var gplay = require('google-play-scraper');
+
+gplay.list({
+    category: gplay.category.GAME_ACTION,
+    collection: gplay.collection.TOP_FREE,
+    num: 2
+  })
+  .then(console.log, console.log);
